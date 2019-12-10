bbf668a6a2d57026ab5897e9708387f98b876e4e
“lyhisphper <894806814@qq.com>
2019-12-10 12:02:01 +0800

    update

--- app/Admin/Controllers/UserController.php
+++ app/Admin/Controllers/UserController.php
@@ -19,7 +19,7 @@ class UserController extends AdminController
      *
      * @var string
      */
-    protected $title = 'App\User';
+    protected $title = '用户管理';
 
     /**
      * Make a grid builder.
@@ -28,21 +28,16 @@ class UserController extends AdminController
      */
     protected function grid()
     {
-        $grid = new Grid(new User);
+        $grid = new Grid(new User());
 
         $grid->column('id', __('Id'));
-        $grid->column('name', __('Name'));
-        $grid->column('email', __('Email'));
-        $grid->column('email_verified_at', __('Email verified at'));
-        $grid->column('password', __('Password'));
-        $grid->column('remember_token', __('Remember token'));
-        $grid->column('created_at', __('Created at'));
-        $grid->column('updated_at', __('Updated at'));
-        $grid->column('avatar', __('Avatar'));
-        $grid->column('mobile', __('Mobile'));
-        $grid->column('gender', __('Gender'));
-        $grid->column('joinip', __('Joinip'));
-        $grid->column('status', __('Status'));
+        $grid->column('name', __('admin.name'));
+        $grid->column('email', __('admin.email'));
+        $grid->column('avatar', __('admin.avatar'))->image('',50,50);
+        $grid->column('mobile', __('admin.mobile'))->replace([0 => '未设置']);
+        $grid->column('gender', __('admin.gender'))->using([0 => '保密', 1 => '男', 2 => '女']);
+        $grid->column('status', __('admin.status'))->using([0 => '已删除', 1 => '正常', 2 => '禁用']);
+        $grid->column('created_at', __('admin.created_at'));
 
         return $grid;
     }
@@ -58,18 +53,15 @@ class UserController extends AdminController
         $show = new Show(User::findOrFail($id));
 
         $show->field('id', __('Id'));
-        $show->field('name', __('Name'));
-        $show->field('email', __('Email'));
-        $show->field('email_verified_at', __('Email verified at'));
-        $show->field('password', __('Password'));
-        $show->field('remember_token', __('Remember token'));
-        $show->field('created_at', __('Created at'));
-        $show->field('updated_at', __('Updated at'));
-        $show->field('avatar', __('Avatar'));
-        $show->field('mobile', __('Mobile'));
-        $show->field('gender', __('Gender'));
-        $show->field('joinip', __('Joinip'));
-        $show->field('status', __('Status'));
+        $show->field('name', __('admin.name'));
+        $show->field('email', __('admin.email'));
+        $show->avatar(__('admin.avatar'))->image();
+        $show->field('mobile', __('admin.mobile'));
+        $show->field('gender', __('admin.gender'))->using([0 => '保密', 1 => '男', 2 => '女']);
+        $show->field('joinip', __('admin.joinip'));
+        $show->field('status', __('admin.status'))->using([0 => '删除', 1 => '正常', 2 => '禁用']);
+        $show->field('created_at', __('admin.created_at'));
+        $show->field('updated_at', __('admin.updated_at'));
 
         return $show;
     }
@@ -83,17 +75,22 @@ class UserController extends AdminController
     {
         $form = new Form(new User);
 
-        $form->text('name', __('Name'));
-        $form->email('email', __('Email'));
-        $form->datetime('email_verified_at', __('Email verified at'))->default(date('Y-m-d H:i:s'));
-        $form->password('password', __('Password'));
-        $form->text('remember_token', __('Remember token'));
-        $form->image('avatar', __('Avatar'));
-        $form->number('mobile', __('Mobile'));
-        $form->switch('gender', __('Gender'));
-        $form->text('joinip', __('Joinip'));
-        $form->switch('status', __('Status'))->default(1);
+        $form->text('name', __('admin.name'));
+        $form->email('email', __('admin.email'));
+        $form->password('password', __('admin.password'));
+        $form->image('avatar', __('admin.avatar'));
+        $form->text('mobile', __('admin.mobile'))->pattern('[1][3,4,5,7,8,9][0-9]{9}');
+        $form->select('gender', __('admin.gender'))->default(0)->options([0 => '保密', 1 => '男', 2 => '女']);
+        $form->text('joinip', __('admin.joinip'));
+        $form->select('status', __('admin.status'))->default(1)->options([0 => '删除', 1 => '正常', 2 => '禁用']);
 
+        $form->saving(function (Form $form) {
+            if ($form->password && $form->model()->password != $form->password) {
+                $form->password = bcrypt($form->password);
+            } else {
+                $form->password = $form->model()->password;
+            }
+        });
         return $form;
     }
 }
